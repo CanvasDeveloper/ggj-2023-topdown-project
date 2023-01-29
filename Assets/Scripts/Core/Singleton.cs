9811b7b83dc65fd664ab76e22d103cfe7ce4de36
CanvasDEV <erickcanvas@gmail.com>
2023-01-29 20:18:45 -0300

    Added singleton

---
+++ Assets/Scripts/Core/Singleton.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public abstract class Singleton<T> : MonoBehaviour where T : Component
+{
+    public static T instance;
+
+    protected virtual void Awake()
+    {
+        if (instance != null)
+        {
+            string typename = typeof(T).Name;
+            Debug.LogWarning($"More that one instance of {typename} found.");
+            Destroy(this);
+        }
+
+        instance = this as T;
+
+        DontDestroyOnLoad(gameObject);
+    }
+}
