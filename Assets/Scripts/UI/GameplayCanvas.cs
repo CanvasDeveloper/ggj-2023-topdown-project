f7d5a4042c2cf556df8f4a9b0281f0dd7fd8311c
CanvasDEV <erickcanvas@gmail.com>
2023-01-30 08:31:11 -0300

    Update GameplayCanvas.cs

--- Assets/Scripts/UI/GameplayCanvas.cs
+++ Assets/Scripts/UI/GameplayCanvas.cs
@@ -4,11 +4,17 @@ using UnityEngine.UI;
 public class GameplayCanvas : MonoBehaviour
 {
     [SerializeField] private GameObject pausePanel;
+    [SerializeField] private GameObject deathPanel;
     [SerializeField] private Button backgroundButton;
+
+    /// <summary>
+    /// Interface do sistema de vida
+    /// </summary>
+    private IDamageable playerHealthSystem;
 
     private void Start()
     {
-        pausePanel.SetActive(false);
+        DisableAll();
 
         if(backgroundButton)
             backgroundButton.onClick.AddListener(HandlePauseUIButton);
@@ -17,11 +23,21 @@ public class GameplayCanvas : MonoBehaviour
     private void OnEnable()
     {
         GameManager.Instance.OnPauseStatusChange += HandlePauseUI;
+
+        playerHealthSystem = FindObjectOfType<PlayerController>().gameObject.GetComponent<IDamageable>();
+
+        //Registrando os eventos
+        playerHealthSystem.OnChangeHealth += UpdateHealthBar;
+        playerHealthSystem.OnDie += OpenDeathScreen;
     }
 
     private void OnDisable()
     {
         GameManager.Instance.OnPauseStatusChange -= HandlePauseUI;
+
+        //removendo os eventos
+        playerHealthSystem.OnChangeHealth -= UpdateHealthBar;
+        playerHealthSystem.OnDie -= OpenDeathScreen;
     }
 
     private void HandlePauseUIButton()
@@ -29,11 +45,43 @@ public class GameplayCanvas : MonoBehaviour
         GameManager.Instance.PauseResume();
     }
 
+    /// <summary>
+    /// Desabilita ou habilita o painel de pause de acordo com o valor
+    /// </summary>
+    /// <param name="value">Estado de pausa</param>
     private void HandlePauseUI(bool value)
     {
         pausePanel.SetActive(value);
 
         if (value && backgroundButton)
             backgroundButton.Select();
+    }
+
+    /// <summary>
+    /// Atualiza a barra de vida do player na UI
+    /// </summary>
+    /// <param name="current">Valor atual</param>
+    /// <param name="max">Valor maximo</param>
+    private void UpdateHealthBar(float current, float max)
+    {
+        var percent = current / max;
+    }
+
+    /// <summary>
+    /// Abre o painel de morte
+    /// </summary>
+    private void OpenDeathScreen()
+    {
+        DisableAll();
+        deathPanel.SetActive(true);
+    }
+
+    /// <summary>
+    /// Desativa todos os paineis
+    /// </summary>
+    private void DisableAll()
+    {
+        pausePanel.SetActive(false);
+        deathPanel.SetActive(false);
     }
 }
