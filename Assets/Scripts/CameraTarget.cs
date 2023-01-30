9c9c0c6c37b59fe01518b0ec91460464cadaf673
CanvasDEV <erickcanvas@gmail.com>
2023-01-30 14:52:49 -0300

    Added camera target

---
+++ Assets/Scripts/CameraTarget.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTarget : MonoBehaviour
+{
+    [SerializeField] private Camera cam;
+    [SerializeField] private Transform player;
+    [SerializeField] private float threshold;
+
+    private void Update()
+    {
+        var mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+        var targetPos = (player.position + mousePos) / 2f;
+
+        targetPos.x = Mathf.Clamp(targetPos.x, -threshold + player.position.x, threshold + player.position.x);
+        targetPos.y = Mathf.Clamp(targetPos.y, -threshold + player.position.y, threshold + player.position.y);
+
+        transform.position = targetPos;
+    }
+}
