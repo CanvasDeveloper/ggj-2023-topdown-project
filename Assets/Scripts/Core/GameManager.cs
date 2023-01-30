c78d5951910c04b336d75509357b5945df034729
CanvasDEV <erickcanvas@gmail.com>
2023-01-29 21:01:06 -0300

    Added game manager

---
+++ Assets/Scripts/Core/GameManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class GameManager : Singleton<GameManager>
+{
+    public event Action<bool> OnPauseStatusChange;
+    public bool Paused { get; private set; }
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0;
+        OnPauseStatusChange?.Invoke(true);
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1;
+        OnPauseStatusChange?.Invoke(false);
+    }
+}
