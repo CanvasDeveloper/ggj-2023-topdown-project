3db1606d9ce2440c211f6dde8c26879c93318c4e
CanvasDEV <erickcanvas@gmail.com>
2023-01-29 21:15:57 -0300

    Update InputReference.cs

--- Assets/Scripts/InputReference.cs
+++ Assets/Scripts/InputReference.cs
@@ -2,14 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public class InputButton
+{
+    public bool IsPressed;
+}
+
 public class InputReference : MonoBehaviour
 {
     //quero pegar o valor, mas não deixo ninguém de fora atualizar
     public Vector2 Movement { get; private set; } = Vector2.zero;
+    public InputButton PauseButton { get; private set; } = new InputButton();
 
     private void Update()
     {
         UpdateMovementValue();
+        UpdatePauseValue();
     }
 
     private void UpdateMovementValue()
@@ -18,5 +25,23 @@ public class InputReference : MonoBehaviour
         var inputY = Input.GetAxisRaw("Vertical");
 
         Movement = new Vector2(inputX, inputY).normalized;
+    }
+
+    private void UpdatePauseValue()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.P))
+        {
+            PauseButton.IsPressed = true;
+            StartCoroutine(ResetButton(PauseButton));
+        }
+
+        if (Input.GetKeyUp(KeyCode.Escape) || Input.GetKeyUp(KeyCode.P))
+            PauseButton.IsPressed = false;
+    }
+
+    private IEnumerator ResetButton(InputButton button)
+    {
+        yield return new WaitForEndOfFrame();
+        button.IsPressed = false;
     }
 }
