50cb4c3b550bf561ce02f70ff1f1c8f3bd162c80
CanvasDEV <erickcanvas@gmail.com>
2023-01-29 21:52:26 -0300

    Fixed game manager

--- Assets/Scripts/Core/GameManager.cs
+++ Assets/Scripts/Core/GameManager.cs
@@ -11,16 +11,18 @@ public class GameManager : Singleton<GameManager>
     /// </summary>
     public bool CanPause = true;
 
-    public void PauseGame()
+    private void PauseGame()
     {
+        Paused = true;
         Time.timeScale = 0;
-        OnPauseStatusChange?.Invoke(true);
+        OnPauseStatusChange?.Invoke(Paused);
     }
 
-    public void ResumeGame()
+    private void ResumeGame()
     {
+        Paused = false;
         Time.timeScale = 1;
-        OnPauseStatusChange?.Invoke(false);
+        OnPauseStatusChange?.Invoke(Paused);
     }
 
     public void PauseResume()
@@ -29,8 +31,8 @@ public class GameManager : Singleton<GameManager>
             return;
 
         if (Paused)
-            PauseGame();
-        else
             ResumeGame();
+        else
+            PauseGame();
     }
 }
