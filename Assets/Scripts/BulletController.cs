30d90d4f4a5cbe6217beddfa24205828db1377bc
CanvasDEV <erickcanvas@gmail.com>
2023-01-31 10:11:44 -0300

    Fixed bullet destroy

--- Assets/Scripts/BulletController.cs
+++ Assets/Scripts/BulletController.cs
@@ -5,16 +5,23 @@ using UnityEngine;
 public class BulletController : MonoBehaviour
 {
     [SerializeField] private float speedBullet = 4f;
+    [SerializeField] private float lifeTime = 4f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(IE_LifeTime());
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector3.up * speedBullet * Time.deltaTime);
+    }
+
+    private IEnumerator IE_LifeTime()
+    {
+        yield return new WaitForSeconds(lifeTime);
+        Destroy(gameObject);
     }
 }
