8dbb1e1688aa138c19965c97fb0fd859d860bb0b
CanvasDEV <erickcanvas@gmail.com>
2023-01-31 16:31:09 -0300

    Added flip and distance behaviour

--- Assets/Scripts/EnemyMoviment.cs
+++ Assets/Scripts/EnemyMoviment.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class EnemyMoviment : MonoBehaviour
 {
     [SerializeField] float speed;
+    [SerializeField] private float distanceToTree;
 
     //Colocado para fazer movimentação de teste
     [SerializeField] Transform treeMother;
+    [SerializeField] private bool isLookingLeft;
 
     private void OnEnable()
     {
@@ -15,6 +17,33 @@ public class EnemyMoviment : MonoBehaviour
     }
     private void Update()
     {
+        Flip();
+
+        if (Vector3.Distance(transform.position, treeMother.position) < distanceToTree)
+        {
+            //perto da arvore
+            
+
+
+            return;
+        }
+
         transform.position = Vector3.MoveTowards(transform.position, treeMother.position, speed * Time.deltaTime);
+    }
+
+    private void Flip()
+    {
+        if (transform.position.x > treeMother.position.x && isLookingLeft)
+        {
+            isLookingLeft = false;
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+        }
+
+        if (transform.position.x < treeMother.position.x && !isLookingLeft)
+        {
+            isLookingLeft = true;
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+
     }
 }
