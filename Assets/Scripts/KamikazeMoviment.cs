f51238a98d6b9d1bcb99baa4521958b56a0e1ceb
fishbr <34253228+fishbr@users.noreply.github.com>
2023-02-03 22:01:50 -0300

    kamikaze

---
+++ Assets/Scripts/KamikazeMoviment.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KamikazeMoviment : MonoBehaviour
+{
+
+    [SerializeField] float speed;
+    [SerializeField] private float distanceToTree;
+
+    //Colocado para fazer movimentação de teste
+    [SerializeField] Transform treeMother;
+    [SerializeField] private bool isLookingLeft;
+
+    [SerializeField] private float damage = 1;
+ 
+
+
+    private bool canHit = true;
+
+    private void OnEnable()
+    {
+        treeMother = TreeController.Instance.transform;
+
+    }
+    private void Update()
+    {
+        Flip();
+
+        if (Vector3.Distance(transform.position, treeMother.position) < distanceToTree)
+        {
+            //perto da arvore
+            if (canHit)
+            {
+                TreeController.Instance.TakeDamage(damage);
+                Destroy(gameObject);
+               
+            }
+
+            return;
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, treeMother.position, speed * Time.deltaTime);
+    }
+
+   
+
+    private void Flip()
+    {
+        if (transform.position.x > treeMother.position.x && isLookingLeft)
+        {
+            isLookingLeft = false;
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+        }
+
+        if (transform.position.x < treeMother.position.x && !isLookingLeft)
+        {
+            isLookingLeft = true;
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+
+    }
+}
